ab7dcdb1e90a9daec6704d62c91a4d5cefcc1f7f
Hannah Cussen <hcussen6@gmail.com>
2020-11-22 23:14:58 -0700

    extracting components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,64 @@
+import React, { useState } from "react";
 import "./App.css";
 import { Header } from "semantic-ui-react";
+import { TextField } from "@material-ui/core";
+import TempoSlider from "./components/TempoSlider/TempoSlider";
 import FieldInput from "./components/FieldInput/FieldInput";
 
 function App() {
+  // location variables
+  const [drumX, setDrumX] = useState(0);
+  const [drumY, setDrumY] = useState(0);
+
+  const [hornX, setHornX] = useState(0);
+  const [hornY, setHornY] = useState(108);
+
+  const [boxY, setBoxY] = useState(5);
+  const [boxZ, setBoxZ] = useState(5);
+
+  // tempo
+  const [tempo, setTempo] = useState(160);
+
   return (
     <div className="app-container">
       <Header as="h1">Sound Delay</Header>
-      <FieldInput />
+
+      <FieldInput
+        drumX={drumX}
+        setDrumX={setDrumX}
+        drumY={drumY}
+        setDrumY={setDrumY}
+        hornX={hornX}
+        setHornX={setHornX}
+        hornY={hornY}
+        setHornY={setHornY}
+      />
+
+      <form>
+        <div className="number-inputs">
+          <TextField
+            id="boxY"
+            label="Box to F.Sideline (yards)"
+            type="number"
+            variant="outlined"
+            value={boxY}
+            onChange={setBoxY}
+          />
+          {/* </div> */}
+          {/* <div className="number-input"> */}
+          <TextField
+            id="boxZ"
+            label="Box Height (yards)"
+            type="number"
+            variant="outlined"
+            value={boxZ}
+            onChange={setBoxZ}
+          />
+        </div>
+
+        <br />
+        <TempoSlider tempo={tempo} handleChange={setTempo} />
+      </form>
     </div>
   );
 }
--- src/components/FieldInput/FieldInput.js
+++ src/components/FieldInput/FieldInput.js
@@ -1,31 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import "./FieldInput.css";
 import Draggable from "react-draggable";
-import { useForm } from "react-hook-form";
-import { TextField } from "@material-ui/core";
 import Marcher from "../Marcher/Marcher";
-import TempoSlider from "../TempoSlider/TempoSlider";
 import drill_chart from "../../assets/drill_chart.png";
 /** will compose draggable marchers onto the droppable field component
  *
  */
-const FieldInput = () => {
-  const { register, handleSubmit, watch, errors } = useForm();
-  const onSubmit = (data) => console.log(data);
-
-  // location variables
-  const [drumX, setDrumX] = useState(0);
-  const [drumY, setDrumY] = useState(0);
-
-  const [hornX, setHornX] = useState(0);
-  const [hornY, setHornY] = useState(108);
-
-  const [boxY, setBoxY] = useState(5);
-  const [boxZ, setBoxZ] = useState(5);
-
-  // tempo
-  const [tempo, setTempo] = useState(160);
-
+const FieldInput = ({
+  drumX,
+  setDrumX,
+  drumY,
+  setDrumY,
+  hornX,
+  setHornX,
+  hornY,
+  setHornY,
+}) => {
   /* handle input events */
   const handleDragDrum = (e, ui) => {
     setDrumX(drumX + ui.deltaX);
@@ -76,27 +66,6 @@ const FieldInput = () => {
       <p>
         Drum Position: ( {drumX}, {drumY} ) Horn Position: ( {hornX}, {hornY} )
       </p>
-
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <TextField
-          id="boxY"
-          label="Box to F.Sideline (yards)"
-          type="number"
-          variant="outlined"
-          ref={register}
-        />
-        <br />
-        <TextField
-          id="boxZ"
-          label="Box Height (yards)"
-          type="number"
-          variant="outlined"
-          ref={register}
-        />
-
-        <br />
-        <TempoSlider tempo={tempo} handleChange={setTempo} />
-      </form>
     </div>
   );
 };
